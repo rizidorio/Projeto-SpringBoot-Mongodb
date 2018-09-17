ef445856c395c02f75332b87f124cc8099e8c55a
rizidorio <rferreira013@hotmail.com>
2018-09-17 11:28:54 -0300

    Padrão DTO e busca por ID

--- src/main/java/com/ricardoizidorio/springbootmongodb/service/UserService.java
+++ src/main/java/com/ricardoizidorio/springbootmongodb/service/UserService.java
@@ -1,12 +1,14 @@
 package com.ricardoizidorio.springbootmongodb.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ricardoizidorio.springbootmongodb.domain.User;
 import com.ricardoizidorio.springbootmongodb.repository.UserRepository;
+import com.ricardoizidorio.springbootmongodb.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,12 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+		
 	}
 
 }
