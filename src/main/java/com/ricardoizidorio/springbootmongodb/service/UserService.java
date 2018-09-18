5c3a15c6ecea242be180636ff66021127c80a7f2
rizidorio <rferreira013@hotmail.com>
2018-09-18 11:09:22 -0300

    Delete e Update de usuários

--- src/main/java/com/ricardoizidorio/springbootmongodb/service/UserService.java
+++ src/main/java/com/ricardoizidorio/springbootmongodb/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ricardoizidorio.springbootmongodb.domain.User;
+import com.ricardoizidorio.springbootmongodb.dto.UserDTO;
 import com.ricardoizidorio.springbootmongodb.repository.UserRepository;
 import com.ricardoizidorio.springbootmongodb.service.exception.ObjectNotFoundException;
 
@@ -22,8 +23,31 @@ public class UserService {
 	
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));	
 	}
 
+	public User insert (User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO (UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getNome(), objDto.getEmail());
+	}
 }
